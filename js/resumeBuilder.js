6e6ed6c38f5948de151b3bddde98af9c204e31b1
lubosst <lubosst@gmail.com>
2018-04-04 21:51:21 +0200

    lesson 9.5
    after lesson 9.5

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,21 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ //$("#main").append(["Lubo"]);
+
+//var awesomeThoughts = "I am Lubo and I am AWESOME";
+
+//var funToughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+//console.log(awesomeThoughts);
+
+//$("#main").append([funToughts]);
+
+
+var formattedName = HTMLheaderName.replace("%data%", "Lubo janovicky");
+
+var formattedRole = HTMLheaderName.replace("%data%", "ATT mravec");
+
+
+$("#main").prepend([formattedRole]);
+$("#main").prepend([formattedName]);
