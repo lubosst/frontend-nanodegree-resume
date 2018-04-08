3ba01c2251f3b2017c0481f4d874b57759236042
lubosst <lubosst@gmail.com>
2018-04-08 23:30:28 +0200

    lesson_10
    just started lesoon 10

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,20 +2,78 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
  //$("#main").append(["Lubo"]);
-
 //var awesomeThoughts = "I am Lubo and I am AWESOME";
-
 //var funToughts = awesomeThoughts.replace("AWESOME", "FUN");
-
 //console.log(awesomeThoughts);
-
 //$("#main").append([funToughts]);
+//var formattedName = HTMLheaderName.replace("%data%", "Lubo janovicky");
+//var formattedRole = HTMLheaderName.replace("%data%", "ATT mravec");
+//$("#main").prepend([formattedRole]);
+//$("#main").prepend([formattedName]);
 
+var work = [
+    {
+        employer: "",
+        title: "",
+        location: "",
+        dates: "",
+        description: ""
+    }
+];
 
-var formattedName = HTMLheaderName.replace("%data%", "Lubo janovicky");
+var projects = [
+    {
+        title: "",
+        dates: "",
+        description: "",
+        images: []
+    }
+];
 
-var formattedRole = HTMLheaderName.replace("%data%", "ATT mravec");
+var bio = {
+    name: "Lubomir",
+    role: "",
+    welcomeMessage: "",
+    biotopic: "",
+    contacts: {
+        mobile: "",
+        email: "",
+        github: "",
+        twitter: "",
+        location: "",
+    },
+    skills: ["varenie", "pocuvanie"]
+};
 
+var education = {
+    schools: [
+        {
+            name: "",
+            location: "",
+            degree: "",
+            dates: "",
+            url: "",
+            majors: [],
+        },
+    ],
+    onlineCourses: [
+        {
+            title: "",
+            school: "",
+            dates: "",
+            url: ""
+        }
+    ]
+};
 
-$("#main").prepend([formattedRole]);
-$("#main").prepend([formattedName]);
+//var headerBig = HTMLheaderName.replace("%data%", bio.name);
+$("#header").append([HTMLheaderName.replace("%data%", bio.name)]);
+
+if (bio.skills.length > 0) {
+    $("#header").append([HTMLskillsStart]);
+    var formattedSkill = "";
+    for (var i = 0; i < bio.skills.length; i++) {
+        formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+        $("#skills").append(formattedSkill);
+    };
+};
